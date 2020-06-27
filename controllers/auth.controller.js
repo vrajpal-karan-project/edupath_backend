fcd2d1795895e8958d8ae1c67bd159151d12a144
karan <k123parmar@gmail.com>
2020-06-28 01:50:46 +0530

    Added Signup Code
    Added simple Signup  code without Validations,
    Added User Model,
    Added express-validator for serverSide validations

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,17 @@
+const User = require("../models/user.model");
 
 exports.signup = (req, res) => {
-    res.json({ message: "Signup Route is working" });
+    const user = new User(req.body);
+    user.save((err, user) => {
+        if (err) {
+            console.error("ERROR IN SAVE", err);
+            return res.status(400).json({
+                message: "could not save User",
+                errors: { email: (err.name == "MongoError" && err.code === 11000) ? "Email already Exists" : err.errmsg }
+            });
+        }
+        res.json(user._id);
+    });
 }
 
 exports.login = (req, res) => {
