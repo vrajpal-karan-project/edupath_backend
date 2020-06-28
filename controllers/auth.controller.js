7566e51f68487cb9d2382d5dd64f5e44b6ca1dc1
karan <k123parmar@gmail.com>
2020-06-29 04:43:55 +0530

    Login Functionality
    -Added Login, Logout Controller
    -Added isLoggedin, isAuthenticated, isAdmin middleware for private routes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken"); /*To sign the jwt token with Secret */
+const expressJwt = require("express-jwt"); /* to validate JWT */
 
 // Array of Error to Object 
 const normalErrors = (errors) => {
@@ -34,9 +36,63 @@ exports.signup = (req, res) => {
 
 
 exports.login = (req, res) => {
-    res.json({ message: "Login Route is working" });
-}
+    let errors = validationResult(req);
+    const { email, password } = req.body;
+    if (!errors.isEmpty()) {
+        console.log("=>LOGIN ERROR :", errors);
+        return res.status(422).json({ errors: normalErrors(errors) });
+    }
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            err && console.log("ERROR IN FINDING USEREMAIL", err)
+            return res.status(400).json({ errors: { email: "User email Does not exist" } })
+        }
+        //authenticate method from userSchema
+        if (!user.authenticate(password)) {
+            // If Password NOT correct 
+            return res.status(401).json({ errors: { password: "Email and Password Don't match" } });
+        }
+        // If password is correct , token is generated
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+        //put token in cookie named "token" and set expiry of 24hrs
+        res.cookie("token", token, { expire: new Date(Date.now() + (24 * 60 * 60 * 1000)) });
+
+        //Send Response to FrontEnd
+        const { _id, fullname, email, role } = user;
+        return res.json({ token, user: { _id, fullname, email, role } });
+    })
+};
 
 exports.logout = (req, res) => {
+    res.clearCookie("token");
     res.json({ message: "User Logged out" });
+};
+
+// Check if user LoggedIn for Protected routes
+exports.isLoggedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+});
+
+
+// custom middlewares.
+exports.isAuthenticated = (req, res, next) => {
+    // req.profile obj would be set from frontend
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!checker) {
+        return res.status(403).json({
+            error: "ACCESS DENIED!"
+        });
+    }
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role !== 2) {
+        return res.status(403).json({
+            error: "You are NOT ADMIN ! ACCESS DENIED"
+        });
+    }
+    next();
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 const router = express.Router();
-const { login, signup, logout } = require("../controllers/auth.controller");
+const { check, validationResult } = require("express-validator");
+const { login, logout, signup, isLoggedIn } = require("../controllers/auth.controller");
 
 // Signup route with validations on entry level
 router.post("/signup", [
     check("fullname", "Name should be atleast 3 chars.").isLength({ min: 3 }),
     check("email", "Email is Invalid.").isEmail(),
-    check("password", "password should be atleast 8 chars.").isLength({ min: 8 })
+    check("password", "Password should be atleast 8 chars.").isLength({ min: 8 })
 ], signup);
 
-router.post("/login", login);
+router.post("/login", [
+    check("email", "Email is Invalid.").isEmail(),
+    check("password", "Password should be atleast 8 chars.").isLength({ min: 8 })
+], login);
 
 router.get("/logout", logout);
+
+// Just testing Protected Route that checks if user isLoggedIn
+router.get("/test", isLoggedIn, (req, res) => {
+    // req.auth gives _id of User, cz it's in token
+    res.json(req.auth._id)
+});
 
 module.exports = router;
